614af1d0a95f1e9550db97b0e410d89a8f7f2568
Young Woo Jung <youngwjung@gmail.com>
2019-12-19 19:02:14 +0900

    update test case

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import random
-
-def run_guess(guess, answer):
-    if 0 < guess < 11:
-        if guess == answer:
-            print ('you are a genius!')
+import random 
+ 
+def run_guess(answer, guess): 
+    if 0 < guess < 11: 
+        if guess == answer: 
+            print ('you are a genius!') 
             return True
-    else:
-        print ('I said 1~10')
-        return False
-
-if __name__ == '__main__':
-    answer = random.randint(1, 10)
-    while True:
-        try:
-            guess = int(input('guess a number 1~10:  '))
-            if (run_guess(guess, answer)):
-                break
-        except ValueError:
-            print ('please enter a number')
+    else: 
+        print ('I said 1~10') 
+        return False 
+ 
+if __name__ == '__main__': 
+    answer = random.randint(1, 10) 
+    while True: 
+        try: 
+            guess = int(input('guess a number 1~10:  ')) 
+            if (run_guess(answer, guess)): 
+                break 
+        except ValueError: 
+            print ('please enter a number') 
             continue
